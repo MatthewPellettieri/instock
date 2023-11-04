b39e5d651da6c5ff2bea10974414ffdc1fd82026
Joshua George <joshua.vincent.george@gmail.com>
2023-11-04 16:48:50 -0700

    imported warehouse and inventory page

--- src/App.js
+++ src/App.js
@@ -1,29 +1,31 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header/header";
-
+import WarehousePage from "./pages/WarehousePage/WarehousePage";
+import InventoryPage from "./pages/IventoryPage/InventoryPage";
 
 function App() {
-  return (
-    <>
-    <Header />
-    <BrowserRouter>
-      <Routes>
-		{/* Warehouse */}
-        <Route path="/" element={<Warehouse />}>
-          <Route path="/:id" element={<WarehouseId />} />
-        </Route>
-        <Route path="/editWarehouse/:id" element={<EditWarehouse />}></Route>
-		<Route path="/addWarehouse" element={<AddWarehouse />}></Route>
-		{/* Inventory */}
-        <Route path="/inventory" element={<Inventory />}>
-          <Route path="/inventory/:id" element={<Inventory />} />
-        </Route>
-        <Route path="/editInventory/:id" element={<EditInventory />}></Route>
-		<Route path="/addInventory" element={<AddInventory />}></Route>
-      </Routes>
-    </BrowserRouter>
-    </>
-  );
+	return (
+		<>
+			<Header />
+			<BrowserRouter>
+				<Routes>
+					{/* Warehouse */}
+					<Route path="/" element={<WarehousePage />}>
+						{/* <Route path="/:id" element={<WarehouseId />} /> */}
+					</Route>
+					{/* <Route path="/editWarehouse/:id" element={<EditWarehouse />}></Route>
+		<Route path="/addWarehouse" element={<AddWarehouse />}></Route> */}
+
+					{/* Inventory */}
+					<Route path="/inventory" element={<InventoryPage />}>
+						{/* <Route path="/inventory/:id" element={<Inventory />} /> */}
+					</Route>
+					{/* <Route path="/editInventory/:id" element={<EditInventory />}></Route>
+		<Route path="/addInventory" element={<AddInventory />}></Route>*/}
+				</Routes>
+			</BrowserRouter>
+		</>
+	);
 }
 
 export default App;
