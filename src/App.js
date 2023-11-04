88b04f775c38fe995f8a2151d98a4b6917ada2b0
Joshua George <joshua.vincent.george@gmail.com>
2023-11-04 12:16:12 -0700

    testing git

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
 		<div className="App">
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
-				<p>this is a test</p>
+				<p>this is a test aswell</p>
 				<a
 					className="App-link"
 					href="https://reactjs.org"
