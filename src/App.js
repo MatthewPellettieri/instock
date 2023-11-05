1845a481a94f6cf1cabcd2f80f531a7a01f4af76
Joshua George <joshua.vincent.george@gmail.com>
2023-11-04 17:16:52 -0700

    app.scss imported

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.scss";
 import Header from "./components/Header/header";
 import WarehousePage from "./pages/WarehousePage/WarehousePage";
 import InventoryPage from "./pages/IventoryPage/InventoryPage";
