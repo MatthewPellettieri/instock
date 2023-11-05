3c3c635bd68157c48173ddaf112f576c319fc245
Matthew Pellettieri <mrpellettieri@gmail.comgit config --global user.email mrpellettieri@gmail.com>
2023-11-04 17:44:43 -0700

    majority of header completed

--- src/App.js
+++ src/App.js
@@ -6,22 +6,22 @@ function App() {
   return (
     <>
     <Header />
-    <BrowserRouter>
+    {/* <BrowserRouter>
       <Routes>
-		{/* Warehouse */}
+		
         <Route path="/" element={<Warehouse />}>
           <Route path="/:id" element={<WarehouseId />} />
         </Route>
         <Route path="/editWarehouse/:id" element={<EditWarehouse />}></Route>
 		<Route path="/addWarehouse" element={<AddWarehouse />}></Route>
-		{/* Inventory */}
+		
         <Route path="/inventory" element={<Inventory />}>
           <Route path="/inventory/:id" element={<Inventory />} />
         </Route>
         <Route path="/editInventory/:id" element={<EditInventory />}></Route>
 		<Route path="/addInventory" element={<AddInventory />}></Route>
       </Routes>
-    </BrowserRouter>
+    </BrowserRouter> */}
     </>
   );
 }
--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,9 +1,18 @@
 import './header.scss';
+import logo from '../../assets/Logo/InStock-Logo.svg'
 
 function Header() {
     return (
         <>
-        
+        <header className="background">
+            <div className="logo--container">
+                <img src={logo} alt="instock logo" className="logo" />
+            </div>
+            <nav className="button--container">
+                <button className="nav__button">Warehouses</button>
+                <button className="nav__button">Inventory</button>
+            </nav>
+        </header>
         </>
     )
 }
