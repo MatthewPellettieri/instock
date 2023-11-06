5eb7a82ab2dbf82f9dd1f93672bb0ded3452fff9
Joshua George <joshua.vincent.george@gmail.com>
2023-11-06 00:36:43 -0800

    basic html structure added

--- src/components/InventoryCard/InventoryCard.js
+++ src/components/InventoryCard/InventoryCard.js
@@ -1,7 +1,30 @@
 import "./InventoryCard.scss";
 
-function InventoryCard() {
-	return <div className="inventoryCard">hello world</div>;
+function InventoryCard({ itemName, quantity, status, category }) {
+	return (
+		<div className="inventoryCard">
+			<div className="inventoryCard__container">
+				<div className="inventoryCard__item">
+					<p className="inventoryCard__label">INVENTORY ITEM:</p>
+					<p className="inventoryCard__content">{itemName}</p>
+				</div>
+				<div className="inventoryCard__category">
+					<p className="inventoryCard__label">CATEGORY:</p>
+					<p className="inventoryCard__content">{category}</p>
+				</div>
+			</div>
+			<div className="inventoryCard__status">
+				<div className="inventoryCard__container">
+					<p className="inventoryCard__label">STATUS:</p>
+					<p className="inventoryCard__content">{status}</p>
+				</div>
+				<div className="inventoryCard__quantity">
+					<p className="inventoryCard__label">QTY</p>
+					<p className="inventoryCard__content">{quantity}</p>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default InventoryCard;
