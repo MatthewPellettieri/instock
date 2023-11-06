e2d75b5cb17ef0d878a62b62474df6dd715fd676
Joshua George <joshua.vincent.george@gmail.com>
2023-11-06 00:37:42 -0800

    filled with WIP data, props  to inventoryCard

--- src/components/WarehouseDetails/WarehouseDetails.js
+++ src/components/WarehouseDetails/WarehouseDetails.js
@@ -5,6 +5,19 @@ import backArrow from "../../assets/Icons/arrow_back-24px.svg";
 import InventoryCard from "../InventoryCard/InventoryCard";
 
 function WarehouseDetails() {
+	const {
+		warehouse_name,
+		address,
+		city,
+		country,
+		contact_name,
+		contact_email,
+		contact_phone,
+		contact_position,
+	} = warehouseData[0];
+
+	const warehouseInv = inventoryData.filter((item) => item.warehouse_id === 1);
+
 	return (
 		<div className="warehouseDetails">
 			<div className="warehouse">
@@ -15,32 +28,40 @@ function WarehouseDetails() {
 							src={backArrow}
 							alt="back-arrow"
 						/>
-						<p className="warehouse__header--title">WAREHOUSE NAME</p>
+						<p className="warehouse__header--title">{warehouse_name}</p>
 					</div>
 					<button className="warehouse__header--button">EDIT</button>
 				</div>
 				<div className="warehouse__details">
 					<div className="warehouse__address">
 						<p className="warehouse__label">WAREHOUSE ADDRESS:</p>
-						<p className="warehouse__text">
-							33 pearl street SW, washington USA
-						</p>
+						<p className="warehouse__text">{`${address},${city},${country}`}</p>
 					</div>
 					<div className="warehouse__contacts">
 						<div className="warehouse__contact-name">
 							<p className="warehouse__label">CONTACT NAME:</p>
-							<p className="warehouse__text">graeme lyon</p>
-							<p className="warehouse__text">Warehouse manager</p>
+							<p className="warehouse__text">{contact_name}</p>
+							<p className="warehouse__text">{contact_position}</p>
 						</div>
 						<div className="warehouse__contact-information">
 							<p className="warehouse__label">CONTACT INFORMATION:</p>
-							<p className="warehouse__text">+1(1928)301-0911</p>
-							<p className="warehouse__text">glyon@instock.com</p>
+							<p className="warehouse__text">{contact_phone}</p>
+							<p className="warehouse__text">{contact_email}</p>
 						</div>
 					</div>
 				</div>
 			</div>
-			<InventoryCard />
+			<div className="inventoryList">
+				{warehouseInv.map((item) => (
+					<InventoryCard
+						key={item.id}
+						itemName={item.item_name}
+						quantity={item.quantity}
+						status={item.status}
+						category={item.category}
+					/>
+				))}
+			</div>
 		</div>
 	);
 }
