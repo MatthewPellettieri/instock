794229b76b9ea945dea524b1566db9242e4b838f
Joshua George <joshua.vincent.george@gmail.com>
2023-11-05 23:57:36 -0800

    WarehouseINV renamed to InventoryCard

---
+++ src/components/InventoryCard/InventoryCard.js
@@ -0,0 +1,7 @@
+import "./InventoryCard.scss";
+
+function InventoryCard() {
+	return <div className="inventoryCard">hello world</div>;
+}
+
+export default InventoryCard;
--- src/components/WarehouseInv/WarehouseInv.js
+++
@@ -1,7 +0,0 @@
-import "./WarehouseInv.scss";
-
-function WarehouseInv() {
-	return <div className="warehouseInv">hello world</div>;
-}
-
-export default WarehouseInv;
