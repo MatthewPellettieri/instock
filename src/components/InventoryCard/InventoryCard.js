8d1642749b3f615e21375a7d8e1e33292e0a4b9c
Joshua George <joshua.vincent.george@gmail.com>
2023-11-07 12:27:58 -0800

    inventory__top & btm added

--- src/components/InventoryCard/InventoryCard.js
+++ src/components/InventoryCard/InventoryCard.js
@@ -1,27 +1,35 @@
 import "./InventoryCard.scss";
+import deleteIcon from "../../assets/Icons/delete_outline-24px.svg";
+import editIcon from "../../assets/Icons/edit-24px.svg";
 
 function InventoryCard({ itemName, quantity, status, category }) {
 	return (
 		<div className="inventoryCard">
-			<div className="inventoryCard__container">
-				<div className="inventoryCard__item">
-					<p className="inventoryCard__label">INVENTORY ITEM:</p>
-					<p className="inventoryCard__content">{itemName}</p>
+			<div className="inventoryCard__top">
+				<div className="inventoryCard__container">
+					<div className="inventoryCard__item">
+						<p className="inventoryCard__label">INVENTORY ITEM</p>
+						<p className="inventoryCard__content">{itemName}</p>
+					</div>
+					<div className="inventoryCard__category">
+						<p className="inventoryCard__label">CATEGORY</p>
+						<p className="inventoryCard__text">{category}</p>
+					</div>
 				</div>
-				<div className="inventoryCard__category">
-					<p className="inventoryCard__label">CATEGORY:</p>
-					<p className="inventoryCard__content">{category}</p>
+				<div>
+					<div className="inventoryCard__status">
+						<p className="inventoryCard__label">STATUS</p>
+						<p className="inventoryCard__content">{status}</p>
+					</div>
+					<div className="inventoryCard__quantity">
+						<p className="inventoryCard__label">QTY</p>
+						<p className="inventoryCard__text">{quantity}</p>
+					</div>
 				</div>
 			</div>
-			<div className="inventoryCard__status">
-				<div className="inventoryCard__container">
-					<p className="inventoryCard__label">STATUS:</p>
-					<p className="inventoryCard__content">{status}</p>
-				</div>
-				<div className="inventoryCard__quantity">
-					<p className="inventoryCard__label">QTY</p>
-					<p className="inventoryCard__content">{quantity}</p>
-				</div>
+			<div className="inventoryCard__btm">
+				<img src={deleteIcon} alt="delete-icon" />
+				<img src={editIcon} alt="edit-icon" />
 			</div>
 		</div>
 	);
