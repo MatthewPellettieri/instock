e1aa747e6eb20c265f7ba4dab167add4e8fb9956
BilalHassen <bilalhassen097@gmail.com>
2023-11-10 12:04:42 -0500

    fix html structure and nest styling

--- src/components/WarehouseComponent/WarehouseComponent.js
+++ src/components/WarehouseComponent/WarehouseComponent.js
@@ -14,39 +14,47 @@ export default function warehouseComponent({
   console.log(contactPhone);
   return (
     <>
-      <section className="warehouse__info">
-        <div className="warehouse__name-box">
-          <div className="warehouse__name">
-            <p className="warehouse__name-text">warehouse</p>
-            <div className="warehouse__name-link">
-              <p className="warehouse__location">{warehouseName}</p>
-              <img className="arrow" src={arrow} alt={"blue arrow link"}></img>
+      <section className="warehouseCard">
+        <div className="warehouseCard__flex-container">
+          <div className="warehouseCard__name-box">
+            <div className="warehouseCard__name">
+              <p className="warehouseCard__name-text">warehouse</p>
+              <div className="warehouseCard__name-link">
+                <p className="warehouseCard__location">{warehouseName}</p>
+                <img
+                  className="arrow"
+                  src={arrow}
+                  alt={"blue arrow link"}
+                ></img>
+              </div>
+
+              <div className="warehouseCard__address">
+                <p className="warehouseCard__address-text">address</p>
+                <p className="warehouseCard__location">{`${address}, ${city}, ${country}`}</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="warehouseCard__contact-box">
+            <div className="warehouseCard__contact">
+              <p className="warehouseCard__contact-text">contact name</p>
+              <p className="warehouseCard__contact-name">{contactName}</p>
             </div>
 
-            <div className="warehouse__address">
-              <p className="warehouse__address-text">address</p>
-              <p className="warehouse__location">{`${address}, ${city}, ${country}`}</p>
+            <div className="warehouseCard__contact-info">
+              <p className="warehouseCard__contact-info-text">
+                Contact Information
+              </p>
+              <p className="warehouseCard__contact-number">{contactPhone}</p>
+              <p className="warehouseCard__contact-email">{contactEmail}</p>
             </div>
           </div>
         </div>
-
-        <div className="warehouse__contact-box">
-          <div className="warehouse__contact">
-            <p className="warehouse__contact-text">contact name</p>
-            <p className="warehouse__contact-name">{contactName}</p>
-          </div>
-
-          <div className="warehouse__contact-info">
-            <p className="warehouse__contact-info-text">Contact Information</p>
-            <p className="warehouse__contact-number">{contactPhone}</p>
-            <p className="warehouse__contact-email">{contactEmail}</p>
-          </div>
+        <div className="warehouseCard__icons">
+          <img src={deleteIcon} alt={"delete-icon"} />
+          <img src={editIcon} alt={"edit-icon"} />
         </div>
       </section>
-      <div className="warehouse__icons">
-        <img src={deleteIcon} alt={"delete-icon"} />
-        <img src={editIcon} alt={"edit-icon"} />
-      </div>
     </>
   );
 }
