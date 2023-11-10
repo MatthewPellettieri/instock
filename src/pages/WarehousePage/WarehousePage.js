82820c8030a9c3706b0a7a850b2cadf8bf90ea70
Joshua George <joshua.vincent.george@gmail.com>
2023-11-09 16:33:02 -0800

    comments removed

--- src/pages/WarehousePage/WarehousePage.js
+++ src/pages/WarehousePage/WarehousePage.js
@@ -1,11 +1,9 @@
-// import WarehouseDetails from "../../components/WarehouseDetails/WarehouseDetails";
 import { Link } from "react-router-dom";
 
 function WarehousePage() {
 	return (
 		<div className="warehouse">
 			<p>WAREHOUSE PAGE</p>
-			{/* <WarehouseDetails /> */}
 			<Link to="/warehouseDetails">
 				<p> To warehouseDetails</p>
 			</Link>
