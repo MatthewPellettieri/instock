830a18f807521f0049550af2ccb6d3f557125db5
Matthew Pellettieri <mrpellettieri@gmail.comgit config --global user.email mrpellettieri@gmail.com>
2023-11-11 20:22:24 -0800

    header active button and links added

--- src/App.js
+++ src/App.js
@@ -7,29 +7,30 @@ import WarehouseDetails from "./components/WarehouseDetails/WarehouseDetails";
 import Footer from "./components/Footer/footer";
 
 function App() {
-	return (
-		<>
-			<Header />
-			<div className="whiteBox">
-				<BrowserRouter>
-					<Routes>
-						{/* ------------------Warehouse-------------------------------- */}
-						<Route path="/" element={<WarehousePage />}></Route>
-						<Route path="/warehouseDetails" element={<WarehouseDetails />} />
-						{/* <Route path="/editWarehouse/:id" element={<EditWarehouse />}></Route>
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
+        <div className="whiteBox">
+          <Routes>
+            {/* ------------------Warehouse-------------------------------- */}
+            <Route path="/" element={<WarehousePage />}></Route>
+            <Route path="/warehouseDetails" element={<WarehouseDetails />} />
+            {/* <Route path="/editWarehouse/:id" element={<EditWarehouse />}></Route>
 						<Route path="/addWarehouse" element={<AddWarehouse />}></Route> */}
-						{/* -------------------------Inventory----------------- */}
-						<Route path="/inventory" element={<InventoryPage />}>
-							{/* <Route path="/inventory/:id" element={<Inventory />} /> */}
-						</Route>
-						{/* <Route path="/editInventory/:id" element={<EditInventory />}></Route>
+            {/* -------------------------Inventory----------------- */}
+            <Route path="/inventory" element={<InventoryPage />}>
+              {/* <Route path="/inventory/:id" element={<Inventory />} /> */}
+            </Route>
+            {/* <Route path="/editInventory/:id" element={<EditInventory />}></Route>
 						<Route path="/addInventory" element={<AddInventory />}></Route>*/}
-					</Routes>
-				</BrowserRouter>
-			</div>
-			<Footer />
-		</>
-	);
+          </Routes>
+        </div>
+      </BrowserRouter>
+
+      <Footer />
+    </>
+  );
 }
 
 export default App;
