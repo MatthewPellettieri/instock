79858fd4c7e811794f1936e5a2e512286c75d930
Joshua George <joshua.vincent.george@gmail.com>
2023-11-12 22:41:55 -0800

    header html added

--- src/pages/InventoryPage/InventoryPage.js
+++ src/pages/InventoryPage/InventoryPage.js
@@ -1,7 +1,24 @@
+import "./InventoryPage.scss";
+import inventoryData from "../../test_data/inventory_JSON.json";
+
 function InventoryPage() {
+	// const ifInventoryPage = () => {
+	// 	if (window.location.href === "http://localhost:3000/inventory") {
+	// 		return true;
+	// 	} else {
+	// 		return false;
+	// 	}
+	// };
+
 	return (
-		<div className="inventory">
-			<p>INVENTORY PAGE</p>
+		<div className="inventoryPage">
+			<div className="inventoryPage__header">
+				<div className="inventoryPage__header--container">
+					<p className="inventoryPage__header--title">Inventory</p>
+				</div>
+				<button className="inventoryPage__button"></button>
+				<button className="inventoryPage__button--special">Edit</button>
+			</div>
 		</div>
 	);
 }
