f543d4479cdef5284b15877495a5ea21daf0dc4a
Joshua George <joshua.vincent.george@gmail.com>
2023-11-18 14:12:26 -0800

    added navigate to warehouse/add on button

--- src/pages/WarehousePage/WarehousePage.js
+++ src/pages/WarehousePage/WarehousePage.js
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import WarehouseComponent from "../../components/WarehouseComponent/WarehouseComponent";
 import doubleArrow from "../../assets/Icons/sort-24px.svg";
 import "./WarehousePage.scss";
@@ -6,6 +6,25 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 function WarehousePage() {
+	const navigate = useNavigate();
+	const apiURL = "http://localhost:8080/api/warehouses";
+
+	const [wareData, setWareData] = useState();
+
+	useEffect(() => {
+		axios
+			.get(apiURL)
+			.then((res) => {
+				setWareData(res.data);
+			})
+			.catch((err) => {
+				console.error(err);
+			});
+	}, []);
+
+	const clickHandler = () => {
+		navigate("/warehouse/add");
+	};
 
 	// search functionality
 	const [searchQuery, setSearchQuery] = useState("");
@@ -27,93 +46,77 @@ function WarehousePage() {
 			);
 		});
 	};
-  
-  const apiURL = "http://localhost:8080/api/warehouses";
-	
-	const [wareData, setWareData] = useState();
 
-	useEffect(() => {
-		axios
-			.get(apiURL)
-			.then((res) => {
-				setWareData(res.data);
-			})
-			.catch((err) => {
-				console.error(err);
-			});
-	}, []);
-	
 	if (!wareData) {
 		console.log("loading data");
 	} else {
+		return (
+			<>
+				<div className="warehouse__container">
+					<div className="warehouse__head">
+						<h1 className="warehouse__header">Warehouses</h1>
 
-	return (
-		<>
-			<div className="warehouse__container">
-				<div className="warehouse__head">
-					<h1 className="warehouse__header">Warehouses</h1>
+						<input
+							className="warehouse__search-bar"
+							type="text"
+							placeholder="Search..."
+							onChange={handleSearch}></input>
 
-					<input
-						className="warehouse__search-bar"
-						type="text"
-						placeholder="Search..."
-						onChange={handleSearch}></input>
+						<button className="warehouse__button-blue" onClick={clickHandler}>
+							<p className="warehouse__button-text">+ Add New Warehouse</p>
+						</button>
+					</div>
+				</div>
 
-					<button className="warehouse__button-blue">
-						<p className="warehouse__button-text">+ Add New Warehouse</p>
-					</button>
-				</div>
-			</div>
-
-			<section className="warehouseList">
-				<div className="warehouseList__label one">
-					<p className="warehouseList__name">warehouse</p>
-					<img
-						className="warehouseList__icon"
-						src={doubleArrow}
-						alt="double arrow"></img>
-				</div>
-				<div className="warehouseList__label two">
-					<p className="warehouseList__name">address</p>
-					<img
-						className="warehouseList__icon"
-						src={doubleArrow}
-						alt="double arrow"></img>
-				</div>{" "}
-				<div className="warehouseList__label three">
-					<p className="warehouseList__name">contact name</p>
-					<img
-						className="warehouseList__icon"
-						src={doubleArrow}
-						alt="double arrow"></img>
-				</div>{" "}
-				<div className="warehouseList__label-info">
-					<p className="warehouseList__name-info">contact Information</p>
-					<img
-						className="warehouseList__icon"
-						src={doubleArrow}
-						alt="double arrow"></img>
-				</div>
-				<div className="warehouseList__action">
-					<p className="warehouseList__name action">actions</p>
-				</div>
-			</section>
-			{searchFilter(wareData).map((data) => (
-				<WarehouseComponent
-					key={data.id}
-					id={data.id}
-					warehouseName={data.warehouse_name}
-					contactName={data.contact_name}
-					contactEmail={data.contact_email}
-					contactPhone={data.contact_phone}
-					address={data.address}
-					city={data.city}
-					country={data.country}
-				/>
-			))}
-		</>
-	);
-			}
+				<section className="warehouseList">
+					<div className="warehouseList__label one">
+						<p className="warehouseList__name">warehouse</p>
+						<img
+							className="warehouseList__icon"
+							src={doubleArrow}
+							alt="double arrow"></img>
+					</div>
+					<div className="warehouseList__label two">
+						<p className="warehouseList__name">address</p>
+						<img
+							className="warehouseList__icon"
+							src={doubleArrow}
+							alt="double arrow"></img>
+					</div>{" "}
+					<div className="warehouseList__label three">
+						<p className="warehouseList__name">contact name</p>
+						<img
+							className="warehouseList__icon"
+							src={doubleArrow}
+							alt="double arrow"></img>
+					</div>{" "}
+					<div className="warehouseList__label-info">
+						<p className="warehouseList__name-info">contact Information</p>
+						<img
+							className="warehouseList__icon"
+							src={doubleArrow}
+							alt="double arrow"></img>
+					</div>
+					<div className="warehouseList__action">
+						<p className="warehouseList__name action">actions</p>
+					</div>
+				</section>
+				{searchFilter(wareData).map((data) => (
+					<WarehouseComponent
+						key={data.id}
+						id={data.id}
+						warehouseName={data.warehouse_name}
+						contactName={data.contact_name}
+						contactEmail={data.contact_email}
+						contactPhone={data.contact_phone}
+						address={data.address}
+						city={data.city}
+						country={data.country}
+					/>
+				))}
+			</>
+		);
+	}
 }
 
 export default WarehousePage;
