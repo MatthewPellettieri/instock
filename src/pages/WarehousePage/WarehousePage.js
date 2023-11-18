aa7f058c0dc20f3cc5ed262ae7aa6b5e53070901
Joshua George <joshua.vincent.george@gmail.com>
2023-11-18 15:05:04 -0800

    fixed searchfilter call

--- src/pages/WarehousePage/WarehousePage.js
+++ src/pages/WarehousePage/WarehousePage.js
@@ -6,7 +6,6 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 function WarehousePage() {
-
 	// search functionality
 	const [searchQuery, setSearchQuery] = useState("");
 
@@ -27,30 +26,28 @@ function WarehousePage() {
 			);
 		});
 	};
-  
-  const apiURL = "http://localhost:8080/api/warehouses/";
-	
+
+	const apiURL = "http://localhost:8080/api/warehouses/";
+
 	const [wareData, setWareData] = useState();
-	
+
 	const updatePage = () => {
-		axios
-			.get(apiURL)
-			.then((res) => {
-				setWareData(res.data);
-			})
-	}
+		axios.get(apiURL).then((res) => {
+			setWareData(res.data);
+		});
+	};
 
 	const deleteWarehouse = (id) => {
-		console.log(id)
-        axios
-			.delete(apiURL + id )
+		console.log(id);
+		axios
+			.delete(apiURL + id)
 			.then(() => {
-				updatePage()
+				updatePage();
 			})
 			.catch((err) => {
 				console.error(err);
 			});
-}
+	};
 
 	useEffect(() => {
 		axios
@@ -62,80 +59,80 @@ function WarehousePage() {
 				console.error(err);
 			});
 	}, []);
-	
+
 	if (!wareData) {
 		console.log("loading data");
 	} else {
+		return (
+			<>
+				<div className="warehouse__container">
+					<div className="warehouse__head">
+						<h1 className="warehouse__header">Warehouses</h1>
 
-	return (
-		<>
-			<div className="warehouse__container">
-				<div className="warehouse__head">
-					<h1 className="warehouse__header">Warehouses</h1>
+						<input
+							className="warehouse__search-bar"
+							type="text"
+							placeholder="Search..."
+							onChange={handleSearch}></input>
 
-					<input
-						className="warehouse__search-bar"
-						type="text"
-						placeholder="Search..."></input>
+						<button className="warehouse__button-blue">
+							<p className="warehouse__button-text">+ Add New Warehouse</p>
+						</button>
+					</div>
+				</div>
 
-					<button className="warehouse__button-blue">
-						<p className="warehouse__button-text">+ Add New Warehouse</p>
-					</button>
-				</div>
-			</div>
-
-			<section className="warehouseList">
-				<div className="warehouseList__label one">
-					<p className="warehouseList__name">warehouse</p>
-					<img
-						className="warehouseList__icon"
-						src={doubleArrow}
-						alt="double arrow"></img>
-				</div>
-				<div className="warehouseList__label two">
-					<p className="warehouseList__name">address</p>
-					<img
-						className="warehouseList__icon"
-						src={doubleArrow}
-						alt="double arrow"></img>
-				</div>{" "}
-				<div className="warehouseList__label three">
-					<p className="warehouseList__name">contact name</p>
-					<img
-						className="warehouseList__icon"
-						src={doubleArrow}
-						alt="double arrow"></img>
-				</div>{" "}
-				<div className="warehouseList__label-info">
-					<p className="warehouseList__name-info">contact Information</p>
-					<img
-						className="warehouseList__icon"
-						src={doubleArrow}
-						alt="double arrow"></img>
-				</div>
-				<div className="warehouseList__action">
-					<p className="warehouseList__name action">actions</p>
-				</div>
-			</section>
-			{wareData.map((data) => (
-				<WarehouseComponent
-					key={data.id}
-					id={data.id}
-					warehouseName={data.warehouse_name}
-					contactName={data.contact_name}
-					contactEmail={data.contact_email}
-					contactPhone={data.contact_phone}
-					address={data.address}
-					city={data.city}
-					country={data.country}
-					deleteWarehouse={deleteWarehouse}
-				/>
-			))}
-			<Link to="/warehouseDetails">
-				<p> To warehouseDetails</p>
-			</Link>
-		</>
-	);
-}
+				<section className="warehouseList">
+					<div className="warehouseList__label one">
+						<p className="warehouseList__name">warehouse</p>
+						<img
+							className="warehouseList__icon"
+							src={doubleArrow}
+							alt="double arrow"></img>
+					</div>
+					<div className="warehouseList__label two">
+						<p className="warehouseList__name">address</p>
+						<img
+							className="warehouseList__icon"
+							src={doubleArrow}
+							alt="double arrow"></img>
+					</div>{" "}
+					<div className="warehouseList__label three">
+						<p className="warehouseList__name">contact name</p>
+						<img
+							className="warehouseList__icon"
+							src={doubleArrow}
+							alt="double arrow"></img>
+					</div>{" "}
+					<div className="warehouseList__label-info">
+						<p className="warehouseList__name-info">contact Information</p>
+						<img
+							className="warehouseList__icon"
+							src={doubleArrow}
+							alt="double arrow"></img>
+					</div>
+					<div className="warehouseList__action">
+						<p className="warehouseList__name action">actions</p>
+					</div>
+				</section>
+				{searchFilter(wareData).map((data) => (
+					<WarehouseComponent
+						key={data.id}
+						id={data.id}
+						warehouseName={data.warehouse_name}
+						contactName={data.contact_name}
+						contactEmail={data.contact_email}
+						contactPhone={data.contact_phone}
+						address={data.address}
+						city={data.city}
+						country={data.country}
+						deleteWarehouse={deleteWarehouse}
+					/>
+				))}
+				<Link to="/warehouseDetails">
+					<p> To warehouseDetails</p>
+				</Link>
+			</>
+		);
+	}
 }
 export default WarehousePage;
