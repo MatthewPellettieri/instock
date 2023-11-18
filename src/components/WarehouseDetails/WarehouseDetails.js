5f912d57a12999da4f8b94b7ea3c118a9e2be850
Joshua George <joshua.vincent.george@gmail.com>
2023-11-18 15:28:14 -0800

    linked component to api data

--- src/components/WarehouseDetails/WarehouseDetails.js
+++ src/components/WarehouseDetails/WarehouseDetails.js
@@ -1,5 +1,5 @@
 import "./WarehouseDetails.scss";
-import { Link, useParams } from "react-router-dom";
+import { Link, useParams, useNavigate } from "react-router-dom";
 import InventoryCard from "../InventoryCard/InventoryCard";
 import InventoryHeader from "../InventoryHeader/InventoryHeader";
 import backArrow from "../../assets/Icons/arrow_back-24px.svg";
@@ -8,77 +8,97 @@ import axios from "axios";
 
 function WarehouseDetails() {
 	const { id } = useParams();
+	const navigate = useNavigate();
 
 	const [warehouseData, setWarehouseData] = useState();
-
-	useEffect(() => {
-		axios.get(`http://localhost:8080/api/warehouses/${id}`).then((res) => {
-			setWarehouseData(res.data[0]);
-		});
-	}, []);
-
 	const [currentInv, setCurrentInv] = useState();
 
 	useEffect(() => {
 		axios
-			.get(`http://localhost:8080/api/warehouses/${id}/inventories`)
+			.get(`http://localhost:8080/api/warehouses/${id}`)
 			.then((res) => {
-				setCurrentInv(res.data);
+				setWarehouseData(res.data[0]);
+			})
+			.then(() => {
+				axios
+					.get(`http://localhost:8080/api/warehouses/${id}/inventories`)
+					.then((res) => {
+						setCurrentInv(res.data);
+					});
+			})
+			.catch((err) => {
+				alert(`No warehouse with id:${id} found`);
+				navigate("/");
 			});
 	}, []);
-	return (
-		<div>
-			{/* <div className="warehouseDetails">
-				<div className="warehouseDetails__header">
-					<div className="warehouseDetails__header--container">
-						<Link to="/" className="warehouseDetails__header--link">
-							<img
-								className="warehouseDetails__header--icon"
-								src={backArrow}
-								alt="back-arrow"
-							/>
-						</Link>
-						<p className="warehouseDetails__header--title">{warehouse_name}</p>
+
+	if (!warehouseData || !currentInv) {
+		console.log("loading data");
+	} else {
+		return (
+			<div>
+				<div className="warehouseDetails">
+					<div className="warehouseDetails__header">
+						<div className="warehouseDetails__header--container">
+							<Link to="/" className="warehouseDetails__header--link">
+								<img
+									className="warehouseDetails__header--icon"
+									src={backArrow}
+									alt="back-arrow"
+								/>
+							</Link>
+							<p className="warehouseDetails__header--title">
+								{warehouseData.warehouse_name}
+							</p>
+						</div>
+						<button className="warehouseDetails__button"></button>
+						<button className="warehouseDetails__button--special">Edit</button>
 					</div>
-					<button className="warehouseDetails__button"></button>
-					<button className="warehouseDetails__button--special">Edit</button>
+					<div className="warehouseDetails__details">
+						<div className="warehouseDetails__address">
+							<p className="warehouseDetails__label">WAREHOUSE ADDRESS:</p>
+							<p className="warehouseDetails__text">
+								{`${warehouseData.address}, ${warehouseData.city}, ${warehouseData.country}`}
+							</p>
+						</div>
+						<div className="warehouseDetails__contacts">
+							<div className="warehouseDetails__contact-name">
+								<p className="warehouseDetails__label">CONTACT NAME:</p>
+								<p className="warehouseDetails__text">
+									{warehouseData.contact_name}
+								</p>
+								<p className="warehouseDetails__text">
+									{warehouseData.contact_position}
+								</p>
+							</div>
+							<div className="warehouseDetails__contact-information">
+								<p className="warehouseDetails__label">CONTACT INFORMATION:</p>
+								<p className="warehouseDetails__text">
+									{warehouseData.contact_phone}
+								</p>
+								<p className="warehouseDetails__text">
+									{warehouseData.contact_email}
+								</p>
+							</div>
+						</div>
+					</div>
 				</div>
-				<div className="warehouseDetails__details">
-					<div className="warehouseDetails__address">
-						<p className="warehouseDetails__label">WAREHOUSE ADDRESS:</p>
-						<p className="warehouseDetails__text">
-							{`${address}, ${city}, ${country}`}
-						</p>
-					</div>
-					<div className="warehouseDetails__contacts">
-						<div className="warehouseDetails__contact-name">
-							<p className="warehouseDetails__label">CONTACT NAME:</p>
-							<p className="warehouseDetails__text">{contact_name}</p>
-							<p className="warehouseDetails__text">{contact_position}</p>
-						</div>
-						<div className="warehouseDetails__contact-information">
-							<p className="warehouseDetails__label">CONTACT INFORMATION:</p>
-							<p className="warehouseDetails__text">{contact_phone}</p>
-							<p className="warehouseDetails__text">{contact_email}</p>
-						</div>
-					</div>
+				<div className="inventoryList">
+					{/* inventoryHeader is only visable at 768px and above */}
+					<InventoryHeader />
+					{currentInv.map((item) => (
+						<InventoryCard
+							key={item.id}
+							itemName={item.item_name}
+							quantity={item.quantity}
+							status={item.status}
+							category={item.category}
+						/>
+					))}
 				</div>
 			</div>
-			<div className="inventoryList"> */}
-			{/* inventoryHeader is only visable at 768px and above */}
-			{/* <InventoryHeader />
-				{currentInv.map((item) => (
-					<InventoryCard
-						key={item.id}
-						itemName={item.item_name}
-						quantity={item.quantity}
-						status={item.status}
-						category={item.category}
-					/>
-				))}
-			</div> */}
-		</div>
-	);
+		);
+	}
 }
 
 export default WarehouseDetails;
