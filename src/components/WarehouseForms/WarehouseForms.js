e95290d9f5cd664187e0082084148337f29b8821
Aman Damani <amandamani007@gmail.com>
2023-11-19 02:33:28 -0500

    Small fixes done

--- src/components/WarehouseForms/WarehouseForms.js
+++ src/components/WarehouseForms/WarehouseForms.js
@@ -3,6 +3,7 @@ import { Link, useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import backArrow from "../../assets/Icons/arrow_back-24px.svg";
+import errorIcon from "../../assets/Icons/error-24px.svg";
 
 function WarehouseForms(props) {
 	let navigate = useNavigate();
@@ -25,6 +26,16 @@ function WarehouseForms(props) {
 
 	const {id} = useParams();
 
+	const changeHandler = (event) => {
+		event.preventDefault();
+		if (event.target.value !== ""){
+			let textAreaDOM = document.getElementsByName(event.target.attributes.name.value);
+			textAreaDOM[0].classList.remove("warehouseForms__form--error")
+			let errorTextDOM = document.getElementsByName(`${event.target.attributes.name.value}_error`)
+			errorTextDOM[0].classList.remove("warehouseForms__form--errorText")
+		}
+	}
+
 	const submitHandler = (event) => {
 		event.preventDefault();
 
@@ -38,6 +49,62 @@ function WarehouseForms(props) {
 		dataToSend.contact_phone = event.target.phoneNumber.value;
 		dataToSend.contact_email = event.target.email.value;
 		
+		if (event.target.warehouseName.value === ""){
+			let textAreaDOM = document.getElementsByName("warehouseName")
+			textAreaDOM[0].classList.add("warehouseForms__form--error")
+			let errorTextDOM = document.getElementsByName(`warehouseName_error`)
+			errorTextDOM[0].classList.add("warehouseForms__form--errorText")
+		}
+
+		if (event.target.streetAddress.value === ""){
+			let textAreaDOM = document.getElementsByName("streetAddress")
+			textAreaDOM[0].classList.add("warehouseForms__form--error")
+			let errorTextDOM = document.getElementsByName(`streetAddress_error`)
+			errorTextDOM[0].classList.add("warehouseForms__form--errorText")
+		}
+
+		if (event.target.city.value === ""){
+			let textAreaDOM = document.getElementsByName("city")
+			textAreaDOM[0].classList.add("warehouseForms__form--error")
+			let errorTextDOM = document.getElementsByName(`city_error`)
+			errorTextDOM[0].classList.add("warehouseForms__form--errorText")
+		}
+
+		if (event.target.country.value === ""){
+			let textAreaDOM = document.getElementsByName("country")
+			textAreaDOM[0].classList.add("warehouseForms__form--error")
+			let errorTextDOM = document.getElementsByName(`country_error`)
+			errorTextDOM[0].classList.add("warehouseForms__form--errorText")
+		}
+
+		if (event.target.contactName.value === ""){
+			let textAreaDOM = document.getElementsByName("contactName")
+			textAreaDOM[0].classList.add("warehouseForms__form--error")
+			let errorTextDOM = document.getElementsByName(`contactName_error`)
+			errorTextDOM[0].classList.add("warehouseForms__form--errorText")
+		}
+
+		if (event.target.position.value === ""){
+			let textAreaDOM = document.getElementsByName("position")
+			textAreaDOM[0].classList.add("warehouseForms__form--error")
+			let errorTextDOM = document.getElementsByName(`position_error`)
+			errorTextDOM[0].classList.add("warehouseForms__form--errorText")
+		}
+
+		if (event.target.phoneNumber.value === ""){
+			let textAreaDOM = document.getElementsByName("phoneNumber")
+			textAreaDOM[0].classList.add("warehouseForms__form--error")
+			let errorTextDOM = document.getElementsByName(`phoneNumber_error`)
+			errorTextDOM[0].classList.add("warehouseForms__form--errorText")
+		}
+
+		if (event.target.email.value === ""){
+			let textAreaDOM = document.getElementsByName("email")
+			textAreaDOM[0].classList.add("warehouseForms__form--error")
+			let errorTextDOM = document.getElementsByName(`email_error`)
+			errorTextDOM[0].classList.add("warehouseForms__form--errorText")
+		}
+
 		if (window.location.href.includes("add")) {
 			axios
 			.post(apiURL , dataToSend)
@@ -102,32 +169,40 @@ function WarehouseForms(props) {
 						<h2 className="warehouseForms__form--title">Warehouse Details</h2>
 
 						<h3 className="warehouseForms__form--label">Warehouse Name</h3>
-						<textarea  name="warehouseName" placeholder="Warehouse Name" className="warehouseForms__form--input" defaultValue={warehouseName}></textarea>
+						<textarea  onChange={changeHandler} name="warehouseName" placeholder="Warehouse Name" className="warehouseForms__form--input" defaultValue={warehouseName}></textarea>
+						<p className="warehouseForms__form--noerrorText" name="warehouseName_error"><img src={errorIcon} alt="errorIcon"/>This feild is required</p>
 
 						<h3 className="warehouseForms__form--label">Street Address</h3>
-						<textarea  name="streetAddress" placeholder="Street Address" className="warehouseForms__form--input" defaultValue={address}></textarea>
+						<textarea  onChange={changeHandler} name="streetAddress" placeholder="Street Address" className="warehouseForms__form--input" defaultValue={address}></textarea>
+						<p className="warehouseForms__form--noerrorText" name="streetAddress_error"><img src={errorIcon} alt="errorIcon"/>This feild is required</p>
 
 						<h3 className="warehouseForms__form--label">City</h3>
-						<textarea  name="city" placeholder="City" className="warehouseForms__form--input" defaultValue={city}></textarea>
+						<textarea onChange={changeHandler} name="city" placeholder="City" className="warehouseForms__form--input" defaultValue={city}></textarea>
+						<p className="warehouseForms__form--noerrorText" name="city_error"><img src={errorIcon} alt="errorIcon"/>This feild is required</p>
 
 						<h3 className="warehouseForms__form--label">Country</h3>
-						<textarea  name="country" placeholder="Country" className="warehouseForms__form--input" defaultValue={country}></textarea>
+						<textarea onChange={changeHandler} name="country" placeholder="Country" className="warehouseForms__form--input" defaultValue={country}></textarea>
+						<p className="warehouseForms__form--noerrorText" name="country_error"><img src={errorIcon} alt="errorIcon"/>This feild is required</p>
 					</div>
 					<hr className="warehouseForms__divider"/>
 					<div className="warehouseForms__form">
 						<h2 className="warehouseForms__form--title">Contact Details</h2>
 
 						<h3 className="warehouseForms__form--label">Contact Name</h3>
-						<textarea  name="contactName" placeholder="Contact Name" className="warehouseForms__form--input" defaultValue={contact}></textarea>
+						<textarea onChange={changeHandler} name="contactName" placeholder="Contact Name" className="warehouseForms__form--input" defaultValue={contact}></textarea>
+						<p className="warehouseForms__form--noerrorText" name="contactName_error"><img src={errorIcon} alt="errorIcon"/>This feild is required</p>
 
 						<h3 className="warehouseForms__form--label">Position</h3>
-						<textarea  name="position" placeholder="Position" className="warehouseForms__form--input" defaultValue={position}></textarea>
+						<textarea onChange={changeHandler} name="position" placeholder="Position" className="warehouseForms__form--input" defaultValue={position}></textarea>
+						<p className="warehouseForms__form--noerrorText" name="position_error"><img src={errorIcon} alt="errorIcon"/>This feild is required</p>
 
 						<h3 className="warehouseForms__form--label">Phone Number</h3>
-						<textarea  name="phoneNumber" placeholder="Phone Number" className="warehouseForms__form--input" defaultValue={phone}></textarea>
+						<textarea onChange={changeHandler} name="phoneNumber" placeholder="Phone Number" className="warehouseForms__form--input" defaultValue={phone}></textarea>
+						<p className="warehouseForms__form--noerrorText" name="phoneNumber_error"><img src={errorIcon} alt="errorIcon"/>This feild is required</p>
 
 						<h3 className="warehouseForms__form--label">Email</h3>
-						<textarea  name="email" placeholder="Email" className="warehouseForms__form--input" defaultValue={email}></textarea>
+						<textarea onChange={changeHandler} name="email" placeholder="Email" className="warehouseForms__form--input" defaultValue={email}></textarea>
+						<p className="warehouseForms__form--noerrorText" name="email_error"><img src={errorIcon} alt="errorIcon"/>This feild is required</p>
 					</div>
 				</div>
 				<div className="warehouseForms__buttons">
