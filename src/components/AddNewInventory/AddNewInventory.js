77cc52a5441bfad2f562b3882811dc3bb98c01c4
BilalHassen <bilalhassen097@gmail.com>
2023-11-18 23:05:30 -0500

    wrote logic for submitting form

--- src/components/AddNewInventory/AddNewInventory.js
+++ src/components/AddNewInventory/AddNewInventory.js
@@ -1,9 +1,37 @@
 import backButton from "../../assets/Icons/arrow_back-24px.svg";
 import "./AddNewInventory.scss";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const initialValues = {
+  item_name: "",
+  description: "",
+  category: "",
+  quantity: "",
+  status: "",
+  warehouse_name: "",
+};
 
 function AddInventory() {
   const [quantity, setQuantity] = useState(false);
+  const [warehouseData, setWarehouseData] = useState([]);
+  const [inventoryData, setInventoryData] = useState([]);
+
+  const wareHouseApi = "http://localhost:8080/api/warehouses";
+  const inventoryApi = "http://localhost:8080/api/inventories";
+
+  useEffect(() => {
+    axios.get(`${wareHouseApi}`).then((response) => {
+      console.log(response.data);
+      let wareHouseData = response.data;
+      setWarehouseData(wareHouseData);
+    });
+    axios.get(`${inventoryApi}`).then((response) => {
+      console.log(response.data);
+      let inventoryData = response.data;
+      setInventoryData(inventoryData);
+    });
+  }, []);
 
   const inStockHandler = () => {
     setQuantity(true);
@@ -11,6 +39,31 @@ function AddInventory() {
 
   const outOfStockHandler = () => {
     setQuantity(false);
+  };
+
+  const [values, setValues] = useState(initialValues);
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+
+    setValues({
+      ...values,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(values);
+
+    axios
+      .post("http://localhost:8080/api/inventories/", values)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((err) => {
+        console.log(`${err}`);
+      });
   };
 
   return (
@@ -27,11 +80,13 @@ function AddInventory() {
       <section className="AddInventory__details">
         <div className="AddInventory__details-form-container">
           <h2 className="AddInventory__details-header">Item Details</h2>
-          <form className="AddInventory__details-form">
+          <form className="AddInventory__details-form" onSubmit={handleSubmit}>
             <h3 className="AddInventory__form-header">Item Name</h3>
             <input
               type="text"
-              name="item name"
+              name="item_name"
+              value={values.item_name}
+              onChange={handleInputChange}
               className="AddInventory__details-input-1"
               placeholder="Item Name"
             ></input>
@@ -39,7 +94,9 @@ function AddInventory() {
             <h3 className="AddInventory__form-header">Description</h3>
             <textarea
               type="text"
-              name="item description"
+              name="description"
+              value={values.description}
+              onChange={handleInputChange}
               className="AddInventory__details-input"
               rows="5"
               placeholder="Please enter a brief item description"
@@ -48,10 +105,14 @@ function AddInventory() {
             <div className="AddInventory__details-dropdown">
               <h3 className="AddInventory__form-header">Category</h3>
               <select
-                name="categories"
+                name="category"
+                value={values.category}
+                onChange={handleInputChange}
                 className="AddInventory__details-drop-down"
               >
-                <option value="Please select">Please Select</option>
+                {inventoryData.map((data) => (
+                  <option key={data.id}>{data.category}</option>
+                ))}
               </select>
             </div>
           </form>
@@ -61,14 +122,16 @@ function AddInventory() {
           <h2 className="AddInventory__form-header AddInventory__form-header--two">
             Item Availability
           </h2>
-          <form className="AddInventory__form">
+          <form className="AddInventory__form" onSubmit={handleSubmit}>
             <h5 className="AddInventory__form-header">Status</h5>
             <div className="AddInventory__radio-container">
               <div className="AddInventory__in">
                 <input
-                  name="instock-radio"
-                  value="inStock"
+                  name="status"
                   onClick={inStockHandler}
+                  onChange={handleInputChange}
+                  value="in stock"
+                  checked={values.status === "in stock"}
                   type="radio"
                   className="AddInventory__in-stock"
                 ></input>
@@ -77,9 +140,11 @@ function AddInventory() {
 
               <div className="AddInventory__out">
                 <input
-                  name="instock-radio"
+                  name="status"
                   type="radio"
-                  value="outOfStock"
+                  onChange={handleInputChange}
+                  value="out of stock"
+                  checked={values.status === "out of stock"}
                   onClick={outOfStockHandler}
                   className="AddInventory__in-stock"
                 ></input>
@@ -92,7 +157,9 @@ function AddInventory() {
                 <h3 className="AddInventory__form-header">Quantity</h3>
                 <input
                   type="text"
-                  name="item name"
+                  name="quantity"
+                  value={values.quantity}
+                  onChange={handleInputChange}
                   className="AddInventory__details-input-1"
                   placeholder="0"
                 ></input>
@@ -102,10 +169,14 @@ function AddInventory() {
             <div className="AddInventory__details-dropdown ">
               <h3 className="AddInventory__form-header">Warehouse</h3>
               <select
-                name="categories"
+                name="warehouse_name"
+                value={values.warehouse_name}
+                onChange={handleInputChange}
                 className="AddInventory__details-drop-down"
               >
-                <option value="Electronics">Please select</option>
+                {warehouseData.map((data) => (
+                  <option key={data.id}>{data.warehouse_name}</option>
+                ))}
               </select>
             </div>
           </form>
@@ -114,7 +185,13 @@ function AddInventory() {
       <div className="AddInventory__grey-bar">
         <div className="AddInventory__buttons-container">
           <button className="AddInventory__cancel">Cancel</button>
-          <button className="AddInventory__save">Save</button>
+          <button
+            onClick={handleSubmit}
+            type="submit"
+            className="AddInventory__save"
+          >
+            Save
+          </button>
         </div>
       </div>
     </section>
